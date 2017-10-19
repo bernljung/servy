27a6db9ef43a05272685cacaede4afacc4b30f14
Jimmy Bernljung <jimmy.bernljung@gmail.com>
2017-10-19 08:29:58 +0200

    Add static page for about.html

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -52,11 +52,26 @@ defmodule Servy.Handler do
   end
 
   def route(%{ method: "GET", path: "/about" } = conv) do
-    %{conv | status: 200, resp_body: "contents of a file"}
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("about.html")
+    |> File.read
+    |> handle_file(conv)
   end
 
   def route(%{ path: path } = conv) do
     %{conv | status: 404, resp_body: "404 #{path} not found"}
+  end
+
+  def handle_file({:ok, content}, conv) do
+    %{conv | status: 200, resp_body: content }
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{conv | status: 404, resp_body: "File not found!" }
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{conv | status: 500, resp_body: "File error: #{reason}" }
   end
 
   def format_response(conv) do
@@ -142,7 +157,7 @@ IO.puts response
 
 
 request = """
-GET /bears HTTP/1.1
+GET /about HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
